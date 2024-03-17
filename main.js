488dc2bdfa6bfe3aab9a75114bb36d35c717417c
jeikai <98737616+jeikai@users.noreply.github.com>
2024-03-17 09:50:33 +0700

    study vẽ line

--- main.js
+++ main.js
@@ -2,29 +2,35 @@ import * as THREE from 'three';
 
 // container chứa đối tượng sẽ được render
 const scene = new THREE.Scene();
-// tạo khối lập phương kích thước 1 đơn vị theo chiều rộng, cao và sâu
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// tạo material cho khối
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// khởi tạo đối tượng 3D dùng khối và material
-const cube = new THREE.Mesh( geometry, material );
-// thêm khối vào container
-scene.add( cube );
-// khởi tạo camera góc nhìn với các tham số: góc nhìn 75 độ, tỷ lệ khung hình, mặt cắt gần, mặt cắt xa ( xác định phạm vi hiển thị )
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-camera.position.z = 3;
+// khởi tạo camera góc nhìn với các tham số: góc nhìn 75 độ, tỷ lệ khung hình, mặt cắt gần, mặt cắt xa ( xác định phạm vi hiển thị )
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+// đặt vị trí camera tại 0 0 100 trong scene ( x, y, z)
+camera.position.set( 0, 0, 100 );
+// hướng camera nhìn về điểm 0, 0, 0 trong scene
+camera.lookAt( 0, 0, 0 );
+
 // tạo một render để vẽ scene 3D
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 document.body.appendChild( renderer.domElement );
 
+// tạo material cho line
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+// tạo mảng lưu trữ các điểm
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+
+scene.add( line );
 // tạp vòng lặp animation với 60 fps
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 animate();
